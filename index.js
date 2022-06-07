243dc4894fcf0406a7f76bf6c2201c27428b4b7b
edwardnduru <wambuedd@gmail.com>
2022-06-07 10:02:56 +0300

    commit tree 545dc002dd5bc40ec9201f2c1e75ecba7fb42d7b

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code in this file!
+// const currentUser = 'Grace Hopper';
+// const welcomeMessage = 'Welcome to Flatbook, ';
+// const currentUser = 'Grace Hopper';
+// const welcomeMessage = currentUser;
+// const currentUser = 'Grace Hopper';
+// const welcomeMessage = 'Welcome to Flatbook, currentUser';
+// const currentUser = 'Grace Hopper';
+// const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+// const currentUser = 'Grace Hopper';
+// const welcomeMessage = `Welcome to Flatbook, ${currentUser}`;
+// const currentUser = 'Grace Hopper';
+// const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+// const currentUser = 'Grace Hopper';
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `${welcomeMessage.toUpperCase()}!`;
+// const shortGreeting = 'Welcome, ';
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
